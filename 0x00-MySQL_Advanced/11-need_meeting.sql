590ed50d6a4cee4f1d5b8d87decdc0d648d043f0
dalhatmd <umarwalid4055@gmail.com>
2024-10-17 23:11:01 +0000

    fix task 11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,6 +2,4 @@
 CREATE OR REPLACE VIEW need_meeting AS
 SELECT name
 FROM students
-WHERE score < 80
-  AND (last_meeting IS NULL OR last_meeting < CURRENT_DATE - INTERVAL '1 month');
-
+WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
