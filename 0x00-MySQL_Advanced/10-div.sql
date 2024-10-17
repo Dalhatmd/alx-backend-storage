dee2b21664a01f9dc2e32cc757079ff18e278ec7
dalhatmd <umarwalid4055@gmail.com>
2024-10-17 23:01:17 +0000

    Add task 10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,16 @@
+-- creates a safeDiv function
+DELIMITER //
+--Function creation
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS DECIMAL(10,2)
+DETERMINISTIC
+BEGIN
+    -- Check if b is 0
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END //
+
+DELIMITER ;
