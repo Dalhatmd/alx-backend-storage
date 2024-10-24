5bd771578c2beae6460c2dc533c0fd8708ff8f04
Dalhatmd <umarwalid4055@gmail.com>
2024-10-24 04:39:15 -0700

    Add task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" update """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ updates a topic based on the name """
+    to_update = {"name": name}
+    update = {"$set": {"topics": topics}}
+    result = mongo_collection.update_one(to_update, update)
