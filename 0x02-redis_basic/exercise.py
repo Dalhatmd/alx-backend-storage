80c419ed49f62bd0d629c59eae3beac1befca36a
dalhatmd <umarwalid4055@gmail.com>
2024-10-26 12:49:06 +0100

    Task 1 done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ redis cache """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -10,8 +10,28 @@ class Cache:
         self._redis = redis.Redis(host='localhost', port=6379, db=0)
         self._redis.flushdb()
 
-
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None)
+    -> Union[str, int, bytes, None]:
+        """ converts data to desired format """
+        data = self._redis.get(key)
+
+        if data is None:
+            return None
+
+        if fn:
+            return fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """ converts to string"""
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key:str) -> Optional[int]:
+        """ converts to int """
+        return self.get(key, fn=int)
