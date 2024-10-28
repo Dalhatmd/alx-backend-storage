75fde6948e9e2948561056465d00c58fcc562201
dalhatmd <umarwalid4055@gmail.com>
2024-10-28 14:59:56 +0100

    fix docs and pep8

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,27 +7,32 @@ from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
+    """ counts the calls to method"""
     key = method.__qualname__
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
+        """ wrapper function"""
         self._redis.incr(key)
         return method(self, *args, **kwargs)
 
     return wrapper
 
+
 def call_history(method: Callable) -> Callable:
+    """ saves called methods """
     input_keys = f"{method.__qualname__}:inputs"
     output_keys = f"{method.__qualname__}:outputs"
 
     @wraps(method)
     def wrapper(self, *args):
+        """ wrapper function """
         self._redis.rpush(input_keys, str(args))
         result = method(self, *args)
         self._redis.rpush(output_keys, str(result))
 
         return result
-    
+
     return wrapper
 
 
@@ -44,8 +49,9 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, int, bytes, None]:
-        """ Retrieve data from Redis and apply an optional transformation function """
+    def get(self, key: str, fn: Optional[Callable] = None) \
+            -> Union[str, int, bytes, None]:
+        """ Retrieve data from Redis and apply an transformatiin """
         data = self._redis.get(key)
 
         if data is None:
@@ -64,12 +70,13 @@ class Cache:
         """ Retrieve data as an integer """
         return self.get(key, fn=int)
 
+
 def replay(func):
     """ replays function calls """
     _redis = redis.Redis(host='localhost', port=6379, db=0)
     name = func.__qualname__
 
-    ret_value = _redis.get(f"{name}_calls")
+    ret_value = _redis.get(f"{name}")
     try:
         ret_value = int(ret_value.decode('utf-8'))
     except Exception:
@@ -77,7 +84,7 @@ def replay(func):
 
     inputs = _redis.lrange(f"{name}:inputs", 0, -1)
     outputs = _redis.lrange(f"{name}:outputs", 0, -1)
-    
+
     print(f"{name} was called {ret_value} times")
     for inp, output in zip(inputs, outputs):
         try:
